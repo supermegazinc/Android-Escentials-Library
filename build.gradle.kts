c8a026e481e0eee8ff0bd1141984bd3bac3cadc4
nicof-fulmar <156094170+nicof-fulmar@users.noreply.github.com>
2025-01-05 20:46:57 +0200

    Change Package Location

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/supermegazinc/Android-Escentials-Library")
+            url = uri("https://maven.pkg.github.com/supermegazinc/Android-Libraries")
             credentials {
                 username = githubProperties["gpr.usr"] as String?
                 password = githubProperties["gpr.key"] as String?
